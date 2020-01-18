fc0d6e32ab3cb719d405948432eb652efdfbe39b
digid0c <digidoc22@gmail.com>
2020-01-18 10:18:07 +0200

    Create project skeleton, implement application context bean demo example

---
+++ src/main/java/guru/samples/didemo/DiDemoApplication.java
@@ -0,0 +1,17 @@
+package guru.samples.didemo;
+
+import guru.samples.didemo.controller.MyController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(DiDemoApplication.class, args);
+        MyController controller = (MyController) context.getBean("myController");
+        controller.sayHello();
+    }
+
+}
---
+++ src/main/java/guru/samples/didemo/controller/MyController.java
@@ -0,0 +1,11 @@
+package guru.samples.didemo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+}
