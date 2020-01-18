886b2e913564f1a7a26303220bfdf30bd9ff4eb0
digid0c <digidoc22@gmail.com>
2020-01-18 12:18:55 +0200

    Mark one of the service implementations as primary one

--- src/main/java/guru/samples/didemo/controller/MyController.java
+++ src/main/java/guru/samples/didemo/controller/MyController.java
@@ -1,11 +1,21 @@
 package guru.samples.didemo.controller;
 
+import guru.samples.didemo.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
+    private GreetingService greetingService;
+
+    @Autowired
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public void sayHello() {
         System.out.println("Hello!");
+        System.out.println(greetingService.sayGreeting());
     }
 }
---
+++ src/main/java/guru/samples/didemo/service/PrimaryGreetingService.java
@@ -0,0 +1,14 @@
+package guru.samples.didemo.service;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+public class PrimaryGreetingService implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Hello from Primary Greeting Service!";
+    }
+}
