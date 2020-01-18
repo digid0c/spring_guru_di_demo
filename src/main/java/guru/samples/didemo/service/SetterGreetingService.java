ad0ba7cd6fb5a45e229aae3fa6d19fbcec820144
digid0c <digidoc22@gmail.com>
2020-01-18 11:51:04 +0200

    Introduce qualifiers to use different service implementations

---
+++ src/main/java/guru/samples/didemo/service/SetterGreetingService.java
@@ -0,0 +1,12 @@
+package guru.samples.didemo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SetterGreetingService implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Hello - I was injected by the setter!";
+    }
+}
